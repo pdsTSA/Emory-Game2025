092db28d8bca7026a1da51208722525ff2af9610
Notnek-P <masterkp2006@gmail.com>
2025-05-01 21:26:44 -0400

    opise

--- Assets/Scenes/GameScene/SCRIPT/EnemyAI.cs
+++ Assets/Scenes/GameScene/SCRIPT/EnemyAI.cs
@@ -1,10 +1,11 @@
+﻿
 using UnityEngine;
 using UnityEngine.AI;
 
-public class EnemyAI : MonoBehaviour
+public class EnemyAiTutorial : MonoBehaviour
 {
     public NavMeshAgent agent;
-    
+
     public Transform player;
 
     public LayerMask whatIsGround, whatIsPlayer;
@@ -12,13 +13,14 @@ public class EnemyAI : MonoBehaviour
     public float health;
 
     //Patroling
-    public Vector3 walkPoint
+    public Vector3 walkPoint;
     bool walkPointSet;
     public float walkPointRange;
 
     //Attacking
     public float timeBetweenAttacks;
     bool alreadyAttacked;
+    public GameObject projectile;
 
     //States
     public float sightRange, attackRange;
@@ -26,8 +28,8 @@ public class EnemyAI : MonoBehaviour
 
     private void Awake()
     {
-        player = GameObject.Find("XR Origin (XR Rig)").transform;
-        agent = GetComponent<NavMeshAgent>
+        player = GameObject.Find("PlayerObj").transform;
+        agent = GetComponent<NavMeshAgent>();
     }
 
     private void Update()
@@ -38,30 +40,29 @@ public class EnemyAI : MonoBehaviour
 
         if (!playerInSightRange && !playerInAttackRange) Patroling();
         if (playerInSightRange && !playerInAttackRange) ChasePlayer();
-        if (playerInSightRange && playerInAttackRange) AttackPlayer();
+        if (playerInAttackRange && playerInSightRange) AttackPlayer();
     }
 
     private void Patroling()
     {
         if (!walkPointSet) SearchWalkPoint();
 
-        if (walkPointSet) 
+        if (walkPointSet)
             agent.SetDestination(walkPoint);
-        
+
         Vector3 distanceToWalkPoint = transform.position - walkPoint;
 
         //Walkpoint reached
         if (distanceToWalkPoint.magnitude < 1f)
             walkPointSet = false;
-        
     }
-    
-    private void SearchWalkPoint(){
+    private void SearchWalkPoint()
+    {
         //Calculate random point in range
         float randomZ = Random.Range(-walkPointRange, walkPointRange);
         float randomX = Random.Range(-walkPointRange, walkPointRange);
 
-        walkPoint = new Vector3(transform.poistion.x + randomX, transform.position.y, transform.poistion.z + randomZ);
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
 
         if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
             walkPointSet = true;
@@ -77,47 +78,41 @@ public class EnemyAI : MonoBehaviour
         //Make sure enemy doesn't move
         agent.SetDestination(transform.position);
 
-        transform.LookAt(plyaer);
+        transform.LookAt(player);
 
-        if(!alreadyAttacked)
+        if (!alreadyAttacked)
         {
-            //Attack code here
-
-
-
+            ///Attack code here
+            Rigidbody rb = Instantiate(projectile, transform.position, Quaternion.identity).GetComponent<Rigidbody>();
+            rb.AddForce(transform.forward * 32f, ForceMode.Impulse);
+            rb.AddForce(transform.up * 8f, ForceMode.Impulse);
+            ///End of attack code
 
             alreadyAttacked = true;
-            Invoke(nameof(ResetAttack), timeBetweenAttacks)
+            Invoke(nameof(ResetAttack), timeBetweenAttacks);
         }
     }
-
     private void ResetAttack()
     {
         alreadyAttacked = false;
     }
 
-    public void TakeDamge(int damage)
+    public void TakeDamage(int damage)
     {
-        health -= damage
+        health -= damage;
 
-        if (health <= 0) Invoke(nameof(DestroyEnemy), .5f)
-
+        if (health <= 0) Invoke(nameof(DestroyEnemy), 0.5f);
     }
-
     private void DestroyEnemy()
     {
-        Destroy(gameObject)
+        Destroy(gameObject);
     }
 
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.red;
-        Gizmos.OnDrawGizmosSelected(transform.position, attackRange);
+        Gizmos.DrawWireSphere(transform.position, attackRange);
         Gizmos.color = Color.yellow;
-        Gizmos.OnDrawGizmosSelected(transform.position, sightRange);
-
-
+        Gizmos.DrawWireSphere(transform.position, sightRange);
     }
-
 }
-
